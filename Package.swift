abb4e10605688662f8f0afb0c57e07d8586c9aa9
Justin Donaldson <justin@hushh.ai>
2023-11-08 14:50:39 -0800

    cleanup and fix cicd

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "hushh-whisper",
+    name: "hushh-vibe-catalog-reader",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "hushh-whisper",
-            targets: ["hushh-whisper"]),
+            name: "hushh-vibe-catalog-reader",
+            targets: ["hushh-vibe-catalog-reader"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "hushh-whisper",
+            name: "hushh-vibe-catalog-reader",
             path : "SwiftGenSources"
             ),
         .testTarget(
-            name: "hushh-whisperTests",
-            dependencies: ["hushh-whisper"]),
+            name: "hushh-vibe-catalog-readerTests",
+            dependencies: ["hushh-vibe-catalog-reader"]),
     ]
 )
