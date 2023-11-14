5466330889ec2e4360c2805a859ad346deaebc63
Justin Donaldson <justin@hushh.ai>
2023-11-13 16:31:59 -0800

    add product constructors

--- python/src/hushh/catalog.py
+++ python/src/hushh/catalog.py
@@ -1,25 +1,37 @@
 from hushh.hcf.Catalog import CatalogT
 from hushh.hcf.Product import ProductT
+from hushh.hcf.ProductCharacterization import ProductCharacterizationT
+
 # import numpy.typing as npt
 # import numpy as np
-from dataclasses import dataclass, field
 from typing import Optional
+import uuid
 from .version import VERSION
 
 
-@dataclass
+class ProductCharacterization(ProductCharacterizationT):
+    product_ids: list[str]
+
+
 class Product(ProductT):
-    characterization_ids : list[str] = []
-    def __init__(self, id:str, url:str):
-        self.id = id
+    characterization_ids: list[str]
+
+    def __init__(self, url: str, characterization_ids: Optional[list[str]]):
+        self.id = str(uuid.uuid1())
         self.url = url
-        self.characterization_ids = []
+        if characterization_ids is not None:
+            self.characterization_ids = characterization_ids
+        else:
+            self.characterization_ids = []
 
-@dataclass
+
 class Catalog(CatalogT):
-    products : list[Product] = field(default_factory=list)
-    def __init__(self, id:str, products:Optional[list[Product]]):
-        self.id = id
+    products: list[Product]
+
+    def __init__(self, products: Optional[list[Product]]):
+        self.id = str(uuid.uuid1())
         self.version = VERSION
-
-
+        if products is not None:
+            self.products = products
+        else:
+            self.products = []
